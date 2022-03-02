1e2b46972df8bb293377ac8dd38a1fe97ce91260
Warren Amphlett <warrenamphlett@gmail.com>
2022-03-02 22:34:07 +0000

    add the new files for the new format

---
+++ collection.sh
@@ -0,0 +1,7 @@
+# because this is running inside dot-collections, we have to get the relative path to this file
+COLLECTION=$(dirname $0)
+
+# initialise variables
+. $COLLECTION/vars.sh
+# define some aliases
+. $COLLECTION/.aliases
